85d7d9995448cfd47d8032aa55ef23e7fb64960d
harishkrishna5 <harishkrish09@gmail.com>
2024-12-20 14:49:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum update -y
-yum install -y httpd
+apt update -y
+apt install -y httpd
 systemctl start httpd
 systemctl enable httpd
 echo "<html><h1>Welcome to my Web Server!</h1></html>" > /var/www/html/index.html
